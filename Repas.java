96279bebec7c598e7e75b62a0244ef5c569ddf5a
nolan.almosni.sio@gmail.com <nolan.almosni.sio@gmail.com>
2021-09-15 16:45:51 +0200

    implementation constructeur Repas

--- Repas.java
+++ Repas.java
@@ -5,6 +5,10 @@ public class Repas {
     
     private int numero;
     private LocalDate date;
-    
+
+    public Repas(int numero, LocalDate date){
+        this.numero = numero;
+        this.date = date;
+    }
 
 }
